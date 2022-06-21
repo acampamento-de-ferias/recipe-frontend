6beaa20ab9adbeca96153ad681ec813a621d656a
Gabriel Viegas <gabrielgl13@hotmail.com>
2022-06-20 21:43:44 -0300

    add register

---
+++ src/pages/register/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { RegisterStyled } from "./styles";
+
+const Register = () => {
+    return (
+        <RegisterStyled>
+            <h1> Olá </h1>
+        </RegisterStyled>
+    );
+}
+
+export default Register;
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DesignSystem from "./design-system";
 import Details from "./pages/details";
 import Home from "./pages/home";
+import Register from "./pages/register";
 
 const Routing = () => {
    return(
@@ -11,6 +12,7 @@ const Routing = () => {
             <Route element = { <DesignSystem /> }  path="/" exact />
             <Route element = { <Home /> }  path="/home" />
             <Route element = { <Details /> }  path="/todo" />
+            <Route element = { <Register /> }  path="/register" />
         </Routes>
     </BrowserRouter>
    )
